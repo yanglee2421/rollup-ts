5c729320609065bf70994fa1353ff1ded394a7cf
路明非 <1060761796@qq.com>
2023-05-25 22:16:33 +0800

    sort out code

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import { routerBing, routerDemo, Name, Items } from "@/routers";
 
+const port = 3001;
 const app = express();
 
 app.use("/bing", routerBing);
 app.use("/", routerDemo);
 
-app.listen(3000, () => {
-  console.log("stand by 3000");
+app.listen(port, () => {
+  console.log(`stand by ${port}`);
   console.log(Items);
   console.log(Name);
 });
