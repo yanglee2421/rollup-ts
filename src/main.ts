d45df93a9a8d847d14d87a053f64f73fc8b251f0
ZhangSheng <yanglee2421@gmail.com>
2023-05-30 10:41:42 +0800

    vite后端集成

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import { routerBing, routerDemo, Name, Items } from "@/routers";
+import history from "connect-history-api-fallback";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 app.use("/bing", routerBing);
-app.use("/", routerDemo);
+app.use("/demo", routerDemo);
+app.use(history(), express.static(resolve(__dirname, "../web/dist")));
 
 app.listen(3000, () => {
   console.log("stand by 3000");
