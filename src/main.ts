5f4de052e90878abe3a179d5b0cc5e1e10e64784
ZhangSheng <yanglee2421@gmail.com>
2023-05-30 14:58:01 +0800

    后端集成vite

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ import history from "connect-history-api-fallback";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 
+// Path
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const staticPath = resolve(__dirname, "../public/dist");
+
+// Server
 const port = 3001;
 const app = express();
 
 app.use("/bing", routerBing);
 app.use("/demo", routerDemo);
-app.use(history(), express.static(resolve(__dirname, "../web/dist")));
+app.use("/public/dist", history(), express.static(staticPath));
 
 app.listen(port, () => {
   console.log(`stand by ${port}`);
